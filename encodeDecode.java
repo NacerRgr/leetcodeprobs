9e7320bcf1bdb378a420daa3ac2a3723f5fc941f
naxxer <abdoragragy1@gmail.com>
2025-04-28 01:10:38 +0200

    leetcode probs

--- encodeDecode.java
+++ encodeDecode.java
@@ -5,13 +5,44 @@ public class encodeDecode {
      public String encode(List<String> strs) {
 
 
+        String res = "";
 
 
-        return "";
+        for(String s : strs){
+            
+            int length = s.length();
+            res += length + "#" + s; 
+        }
+
+
+        return res ;
     }
 
     public List<String> decode(String str) {
 
-        return new ArrayList<String>();
+        int i = 0  , j = 0  ;
+        int length = 0 ;
+       
+
+        List<String> arr = new ArrayList<>();
+
+
+        while(i<str.length()){
+        
+            while(str.charAt(j)!='#'){
+                j++;
+            }
+
+        length = Integer.parseInt(str.substring(i, j));
+        i = j+1;
+        String s = str.substring(i, i+length);
+        arr.add(s);
+
+        j = i+length;
+
+        }
+
+
+        return arr;
     }
 }
